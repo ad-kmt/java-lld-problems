3eb1c1c8cc1324d57b00585f09757cde462d93e4
KMT <kumawat.aditya743@gmail.com>
2024-03-03 20:38:41 +0530

    initial commit

--- src/main/java/com/kmt/lld/parkinglot/ParkingLotApp.java
+++ src/main/java/com/kmt/lld/parkinglot/ParkingLotApp.java
@@ -1,6 +1,6 @@
-package com.kmt.lld;
+package com.kmt.lld.parkinglot;
 
-public class Main {
+public class ParkingLotApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
